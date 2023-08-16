eda811b8a761ef25f65a608df7fde0b65bafccce
VaTsaL276 <133655957+VaTsaL276@users.noreply.github.com>
2023-08-16 19:28:05 +0530

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,12 @@
+package prepareStatmentInterface;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		prepareStatmentDemo.addStudent(5,"vatsal");
+		prepareStatmentDemo.showStudent();
+		
+	}
+
+}
